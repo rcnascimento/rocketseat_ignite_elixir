e4d4cd7ee856fcf4c5e0084aaaf2c9bd5b4bcda4
Rodrigo Nascimento <me@rcnascimento.com>
2021-07-31 02:40:25 -0300

    Remove comparação estrita entre inteiros

--- apps/projeto_01/test/sum_list_test.exs
+++ apps/projeto_01/test/sum_list_test.exs
@@ -7,14 +7,14 @@ defmodule SumListTest do
       list1 = [1, 2, 3, 4, 10]
       list2 = [-1, 2, -3, 4, -10]
 
-      assert SumList.call(list1) === 20
-      assert SumList.call(list2) === -8
+      assert SumList.call(list1) == 20
+      assert SumList.call(list2) == -8
     end
 
     test "verifica se zero é retornado para uma lista vazia" do
       list = []
 
-      assert SumList.call(list) === 0
+      assert SumList.call(list) == 0
     end
   end
 end
