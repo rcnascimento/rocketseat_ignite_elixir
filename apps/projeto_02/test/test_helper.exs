bcdb3f5084da40f4d35e0b753dc889802cca4482
Rodrigo Nascimento <me@rcnascimento.com>
2021-07-31 20:33:18 -0300

    Inclui arquivos de exemplo para testes

--- apps/projeto_02/test/test_helper.exs
+++ apps/projeto_02/test/test_helper.exs
@@ -1,1 +1,6 @@
+# Extrai arquivos de exemplo de arquivo ZIP antes de executar os testes.
+# Usa o módulo zip da biblioteca padrão do Erlang:
+# https://erlang.org/doc/man/zip.html#unzip-2
+:zip.unzip('example_files.zip', [{:cwd, ~c'tmp'}])
+
 ExUnit.start()
