0172acdf454715e0236cea2f2d9fba943ed94ac7
Rodrigo Nascimento <me@rcnascimento.com>
2021-07-31 01:20:28 -0300

    Cria esqueleto de projeto baseado em template
    Para resolução e validação do desafio foi fornecido um template contendo
    algumas dependências, testes definidos e um esqueleto básico.

---
+++ apps/desafio_01/lib/list_length.ex
@@ -0,0 +1,19 @@
+defmodule ListLength do
+  @moduledoc """
+  Documentation for `ListLength`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ListLength.call()
+      :world
+
+  """
+  def call() do
+    # TO DO
+    :world
+  end
+end
