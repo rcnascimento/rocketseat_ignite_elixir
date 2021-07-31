478b6e19d5efadade8dc54dbb704402232043c3f
Rodrigo Nascimento <me@rcnascimento.com>
2021-07-31 02:38:51 -0300

    Implementa testes unitários reais para as funções

--- apps/desafio_02/test/list_filter_test.exs
+++ apps/desafio_02/test/list_filter_test.exs
@@ -2,7 +2,21 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.call([]) == 0
+  describe "call/1" do
+    test "retorna a quantidade de numerais ímpares da lista de strings" do
+      list1 = ["1", "3", "5", "99", "xyz", "abc", "101"]
+      list2 = ["kbd", "abd"]
+      list3 = ["2", "80"]
+
+      assert ListFilter.call(list1) == 5
+      assert ListFilter.call(list2) == 0
+      assert ListFilter.call(list3) == 0
+    end
+
+    test "retorna zero para uma lista vazia" do
+      list = []
+
+      assert ListFilter.call(list) == 0
+    end
   end
 end
